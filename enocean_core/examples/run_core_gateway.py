8a8fa95ace42f7ac4c5927dbe6d884778416915f
Henning Kerstan <mail@henningkerstan.de>
2026-02-08 19:05:21 +0100

    reset version

--- enocean_core/examples/run_core_gateway.py
+++ enocean_core/examples/run_core_gateway.py
@@ -9,9 +9,9 @@ async def main():
     print("Starting EnOcean module ...")
     protocol = await ESP3.open_serial_port("/dev/tty.usbserial-EO8FD3C6")
     protocol.add_packet_callback(lambda pkt: print(f"Received {pkt}"))
-    protocol.add_erp1_callback(lambda erp1: print(f" - parsed to {erp1}"))
+    protocol.add_erp1_callback(lambda erp1: print(f"╰─ parsed to {erp1}"))
     protocol.esp3_send_callbacks.append(lambda pkt: print(f"Sending {pkt}"))
-    protocol.response_callbacks.append(lambda resp: print(f"- parsed to {resp}"))
+    protocol.response_callbacks.append(lambda resp: print(f"╰─ parsed to {resp}"))
 
     await protocol.ready
     print("EnOcean module is ready!")
